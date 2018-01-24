f2cbeb5ebd526b94fbdfccc3a8ace3064a7d1e30
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-24 20:55:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,13 +8,5 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  var rank1 = document.getElementsByClassName('ranked-list[0]');
-  var rank2 = document.getElementsByClassName('ranked-list[1]');
-  var length = rank1.length + rank2.length;
-  var element;
-  
-  for(var x = 0; x < length; x++){
-    element = document.getElementsByClassName('ranked-list');
-    document.getElementsByClassName('ranked-list') = parseInt(element + n);
-  }
+  var rankList = document.querySelectorAll('.ranked-list li');
 }
