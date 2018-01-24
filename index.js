96d1b5c1c24cc7ab1cee54c17745b57d21acd723
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-24 20:56:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ function nestedTarget(){
 
 function increaseRankBy(n){
   var rankList = document.querySelectorAll('ul.ranked-list li');
+  
+  for(var x = 0; x < rankList.length; x++){
+    rankList[x].innerHTML = parseInt(rankList[x].innerHTML) + n;
+  }
 }
