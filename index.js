3d25ae162c7c2e51e8446266ae9abacd2089d3ad
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-24 20:37:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
+  var rankLists = document.querySelectorAll('.ranked-list');
   
+  for(var x = 0; x < rankList.length; x++){
+    console.log(rankList[x]);
+  }
 }
