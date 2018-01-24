5ffba0f1d1baac721a126eaddaa1f5d38959c25f
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-24 21:15:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  var element = document.querySelector('#nested div div div div.target');
+  element.parentNode.removeChild(element);
+}
+
+function increaseRankBy(n){
+  var rankList = document.querySelectorAll('ul.ranked-list li');
+  
+  for(var x = 0; x < rankList.length; x++){
+    rankList[x].innerHTML = parseInt(rankList[x].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  var grandNodes = document.getElementById('grand-node').querySelectorAll('div');
+  
+  return grandNodes[grandNodes.length-1];
+}
